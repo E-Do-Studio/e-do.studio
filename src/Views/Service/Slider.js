3366938f43e0347b7d92ecc3fb84773a3e0e4e0e
theodaguier <92515854+theodaguier@users.noreply.github.com>
2023-03-22 11:50:21 +0100

    Committing changes before switching to gallery branch

--- src/Views/Service/Slider.js
+++ src/Views/Service/Slider.js
@@ -1,18 +1,16 @@
-
-
 function Slider() {
-    return (
-        <>
-            <div className="slider">
-                {images.map((image) => (
-                    <div key={image.id} className="imageBox">
-                        <img src={image.src} alt={image.alt} />
-                        <p>{image.title}</p>
-                    </div>
-                ))}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="slider">
+        {images.map((image) => (
+          <div key={image.id} className="imageBox">
+            <img src={image.src} alt={image.alt} />
+            <p>{image.title}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Slider
+export default Slider;
