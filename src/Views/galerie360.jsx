7f6f60c7d93c5031c5e7a684f2c671efe04e66f2
“Ziyad <ziyadclombe.pro@gmail.com>
2025-02-04 14:16:23 +0100

    galerie 360

--- src/Views/galerie360.jsx
+++ src/Views/galerie360.jsx
@@ -1,276 +1,110 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
 import { useMediaQuery } from "@react-hook/media-query";
-import { Waypoint } from "react-waypoint";
-import ScrollContainer from "react-indiana-drag-scroll";
-import { Helmet } from "react-helmet";
-import Lottie from "lottie-react";
-
-import { useLocation } from "react-router-dom";
-
-import anime from "animejs/lib/anime.es.js";
-
-import "./galerie.scss";
-
-import Footer from "../Components/Layout/Footer/footer";
-
-import boutonSliderBlanc from "../Assets/animations/boutonMenuServices.json";
-
-import { useTranslation } from "react-i18next";
-
-// Composants
-
+import { motion } from "framer-motion";
 import GalerieMenu from "./GalerieMenu";
-import IMGPC from "./IMGPC";
-import IMGMobile from "./IMGMobile";
+import "./galerie360.scss";
 
 const Galerie360 = ({ setPageLoad, setSelectedLink }) => {
   const matches = useMediaQuery("only screen and (min-width: 1200px)");
 
-  const [scrollX, setScrollX] = useState(0);
+  const iframeUrls = [
+    "https://api.cappasity.com/api/player/0e20a8ac-e3b4-4762-9022-8dafd774e22f/embedded",
+    "https://api.cappasity.com/api/player/7c8f155a-4944-481e-974b-a2735553a68f/embedded",
+    "https://api.cappasity.com/api/player/5e09c628-8f54-4f02-bfd1-d2c6ab0baf12/embedded",
+    "https://api.cappasity.com/api/player/b565d6d6-3153-40b7-9391-e334d7b71129/embedded",
+    "https://api.cappasity.com/api/player/b3cc1fd4-2e65-4650-ac1f-d4b1403ac276/embedded",
+    "https://api.cappasity.com/api/player/3d0f648b-1cc2-45ab-a3b2-ea38f11f2675/embedded"
+  ];
 
-  const handleScroll = (event) => {
-    setScrollX(event);
-  };
+  const commonIframeParams = "?autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&hidehints=0&language=&autorotate=0&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=1&enableimagezoom=1&zoomquality=1&hidezoomopt=1&arbutton=1";
 
-  const PMS_BoutonPCNextButton = useRef();
-  const PMS_BoutonPCPrecButton = useRef();
-
-  const sliderNavSuiv = () => {
-    PMS_BoutonPCNextButton.current.play();
-    setTimeout(() => {
-      PMS_BoutonPCNextButton.current.stop();
-    }, 600);
-  };
-
-  const sliderNavPrec = () => {
-    PMS_BoutonPCPrecButton.current.play();
-    setTimeout(() => {
-      PMS_BoutonPCPrecButton.current.stop();
-    }, 600);
-  };
-
-  function scrollLeft() {
-    const scrollBox = document.getElementsByClassName("galeriePCWrapper")[0];
-    sliderNavPrec();
-    scrollBox.scrollBy({
-      left: -500,
-      behavior: "smooth",
-    });
-  }
-
-  function scrollRight() {
-    const scrollBox = document.getElementsByClassName("galeriePCWrapper")[0];
-    sliderNavSuiv();
-    scrollBox.scrollBy({
-      left: 500,
-      behavior: "smooth",
-    });
-  }
-
-  useEffect(() => {
-    const scrollBox = document.getElementsByClassName("galeriePCWrapper")[0];
-
-    const keyScroll = (e) => {
-      const key = e.keyCode;
-      if (key == "39") {
-        scrollBox.scrollBy({
-          left: 500,
-          behavior: "smooth",
-        });
-      } else if (key == "37") {
-        scrollBox.scrollBy({
-          left: -500,
-          behavior: "smooth",
-        });
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1
       }
-    };
-
-    document.addEventListener("keydown", keyScroll);
-  }, []);
-
-  useEffect(() => {
-    anime({
-      targets: ".PMS_BoutonPCNextButton",
-      opacity: [0, 1],
-      easing: "easeInOutSine",
-      duration: 500,
-      delay: 300,
-    });
-
-    anime({
-      targets: ".PMS_BoutonPCPrecButton",
-      opacity: [0, 1],
-      easing: "easeInOutSine",
-      duration: 500,
-      delay: 300,
-    });
-
-    //Animation du bouton
-  }, []);
-
-  const videoRef = useRef(null);
-
-  const [imgHover, setImgHover] = useState(false);
-
-  const handleHover = (event) => {
-    videoRef.current.style.opacity = 0.8;
-    videoRef.current.style.filter = "grayscale(1)";
+    }
   };
 
-  const handleMouseOut = (event) => {
-    videoRef.current.style.opacity = 1;
-    videoRef.current.style.filter = "grayscale(0)";
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5
+      }
+    }
   };
-
-  const location = useLocation();
-  const { selectedLink = "360" } = location.state || {};
 
   return (
-    <>
-      <GalerieMenu
-        setPageLoad={setPageLoad}
-        selectedLink={selectedLink}
-        setSelectedLink={setSelectedLink}
-      />
-      {!matches ? (
-        <>
-          <div className="galerieMobile" style={{ paddingLeft: "50px" }}>
-            <iframe
-              width="100vw"
-              height="400px"
-              frameborder="0"
-              allowfullscreen
-              mozallowfullscreen="true"
-              webkitallowfullscreen="true"
-              style={{ border: 0 }}
-              src="https://api.cappasity.com/api/player/0e20a8ac-e3b4-4762-9022-8dafd774e22f/embedded?autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=1&enableimagezoom=1&zoomquality=1&hidezoomopt=1&arbutton=1"
-            />
-            <iframe
-              width="100vw"
-              height="400px"
-              frameborder="0"
-              allowfullscreen
-              mozallowfullscreen="true"
-              webkitallowfullscreen="true"
-              style={{ border: 0 }}
-              src="https://api.cappasity.com/api/player/7c8f155a-4944-481e-974b-a2735553a68f/embedded?autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=1&enableimagezoom=1&zoomquality=1&hidezoomopt=1&arbutton=1"
-            />
-            <iframe
-              width="100vw"
-              height="400px"
-              frameborder="0"
-              allowfullscreen
-              mozallowfullscreen="true"
-              webkitallowfullscreen="true"
-              style={{ border: 0 }}
-              src="https://api.cappasity.com/api/player/5e09c628-8f54-4f02-bfd1-d2c6ab0baf12/embedded?autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=1&enableimagezoom=1&zoomquality=1&hidezoomopt=1&arbutton=1"
-            />
-            <iframe
-              width="100vw"
-              height="400px"
-              frameborder="0"
-              allowfullscreen
-              mozallowfullscreen="true"
-              webkitallowfullscreen="true"
-              style={{ border: 0 }}
-              src="https://api.cappasity.com/api/player/b565d6d6-3153-40b7-9391-e334d7b71129/embedded?autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=1&enableimagezoom=1&zoomquality=1&hidezoomopt=1&arbutton=1"
-            />
-            <iframe
-              width="100vw"
-              height="400px"
-              frameborder="0"
-              allowfullscreen
-              mozallowfullscreen="true"
-              webkitallowfullscreen="true"
-              style={{ border: 0 }}
-              src="https://api.cappasity.com/api/player/b3cc1fd4-2e65-4650-ac1f-d4b1403ac276/embedded?arbutton=0&autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=0&enableimagezoom=1&zoomquality=1&hidezoomopt=1"
-            />
-            <iframe
-              width="100vw"
-              height="400px"
-              frameborder="0"
-              allowfullscreen
-              mozallowfullscreen="true"
-              webkitallowfullscreen="true"
-              style={{ border: 0 }}
-              src="https://api.cappasity.com/api/player/3d0f648b-1cc2-45ab-a3b2-ea38f11f2675/embedded?arbutton=0&autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=0&enableimagezoom=1&zoomquality=1&hidezoomopt=1"
+    <div className="galeriePC">
+      <div className="galeriePCWrapper">
+        <div className="gallery-layout">
+          <div className="menu-column">
+            <GalerieMenu
+              setPageLoad={setPageLoad}
+              selectedLink="360"
+              setSelectedLink={setSelectedLink}
             />
           </div>
-        </>
-      ) : (
-        <div
-          className="galeriePC"
-          s={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            justifyContent: "center",
-          }}
-        >
-          <iframe
-            width="500px"
-            height="400px"
-            frameborder="0"
-            allowfullscreen
-            mozallowfullscreen="true"
-            webkitallowfullscreen="true"
-            style={{ border: 0 }}
-            src="https://api.cappasity.com/api/player/0e20a8ac-e3b4-4762-9022-8dafd774e22f/embedded?autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=1&enableimagezoom=1&zoomquality=1&hidezoomopt=1&arbutton=1"
-          />
-          <iframe
-            width="500px"
-            height="400px"
-            frameborder="0"
-            allowfullscreen
-            mozallowfullscreen="true"
-            webkitallowfullscreen="true"
-            style={{ border: 0 }}
-            src="https://api.cappasity.com/api/player/7c8f155a-4944-481e-974b-a2735553a68f/embedded?autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=1&enableimagezoom=1&zoomquality=1&hidezoomopt=1&arbutton=1"
-          />
-          <iframe
-            width="500px"
-            height="400px"
-            frameborder="0"
-            allowfullscreen
-            mozallowfullscreen="true"
-            webkitallowfullscreen="true"
-            style={{ border: 0 }}
-            src="https://api.cappasity.com/api/player/5e09c628-8f54-4f02-bfd1-d2c6ab0baf12/embedded?autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=1&enableimagezoom=1&zoomquality=1&hidezoomopt=1&arbutton=1"
-          />
-          <iframe
-            width="500px"
-            height="400px"
-            frameborder="0"
-            allowfullscreen
-            mozallowfullscreen="true"
-            webkitallowfullscreen="true"
-            style={{ border: 0 }}
-            src="https://api.cappasity.com/api/player/b565d6d6-3153-40b7-9391-e334d7b71129/embedded?autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=1&enableimagezoom=1&zoomquality=1&hidezoomopt=1&arbutton=1"
-          />
-          <iframe
-            width="500px"
-            height="400px"
-            frameborder="0"
-            allowfullscreen
-            mozallowfullscreen="true"
-            webkitallowfullscreen="true"
-            style={{ border: 0 }}
-            src="https://api.cappasity.com/api/player/b3cc1fd4-2e65-4650-ac1f-d4b1403ac276/embedded?arbutton=0&autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=0&enableimagezoom=1&zoomquality=1&hidezoomopt=1"
-          />
-          <iframe
-            width="500px"
-            height="400px"
-            frameborder="0"
-            allowfullscreen
-            mozallowfullscreen="true"
-            webkitallowfullscreen="true"
-            style={{ border: 0 }}
-            src="https://api.cappasity.com/api/player/3d0f648b-1cc2-45ab-a3b2-ea38f11f2675/embedded?arbutton=0&autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&storeurl=&hidehints=0&language=&autorotate=0&autorotatetime=10&autorotatedelay=2&autorotatedir=1&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=0&enableimagezoom=1&zoomquality=1&hidezoomopt=1"
-          />
+          
+          <div className="gallery-grid">
+            {!matches ? (
+              <div className="galerieMobile">
+                <motion.div 
+                  className="gallery-360__grid"
+                  variants={containerVariants}
+                  initial="hidden"
+                  animate="visible"
+                >
+                  {iframeUrls.map((url, index) => (
+                    <motion.div 
+                      key={index}
+                      className="gallery-360__item"
+                      variants={itemVariants}
+                    >
+                      <iframe
+                        allowFullScreen
+                        mozallowfullscreen="true"
+                        webkitallowfullscreen="true"
+                        src={`${url}${commonIframeParams}`}
+                      />
+                    </motion.div>
+                  ))}
+                </motion.div>
+              </div>
+            ) : (
+              <div className="gallery-360__grid">
+                <motion.div 
+                  className="gallery-360__grid"
+                  variants={containerVariants}
+                  initial="hidden"
+                  animate="visible"
+                >
+                  {iframeUrls.map((url, index) => (
+                    <motion.div 
+                      key={index}
+                      className="gallery-360__item"
+                      variants={itemVariants}
+                    >
+                      <iframe
+                        allowFullScreen
+                        mozallowfullscreen="true"
+                        webkitallowfullscreen="true"
+                        src={`${url}${commonIframeParams}`}
+                      />
+                    </motion.div>
+                  ))}
+                </motion.div>
+              </div>
+            )}
+          </div>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 };
 
