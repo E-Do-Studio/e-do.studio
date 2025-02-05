f981d033f89c69e7f028863deef6a3bf8c764067
“Ziyad <ziyadclombe.pro@gmail.com>
2025-02-05 14:44:41 +0100

    pre galerie text align left

--- src/Views/galerie360.jsx
+++ src/Views/galerie360.jsx
@@ -8,12 +8,12 @@ const Galerie360 = ({ setPageLoad, setSelectedLink }) => {
   const matches = useMediaQuery("only screen and (min-width: 1200px)");
 
   const iframeUrls = [
+    "https://api.cappasity.com/api/player/b3cc1fd4-2e65-4650-ac1f-d4b1403ac276/embedded",
     "https://api.cappasity.com/api/player/0e20a8ac-e3b4-4762-9022-8dafd774e22f/embedded",
     "https://api.cappasity.com/api/player/7c8f155a-4944-481e-974b-a2735553a68f/embedded",
-    "https://api.cappasity.com/api/player/5e09c628-8f54-4f02-bfd1-d2c6ab0baf12/embedded",
+    "https://api.cappasity.com/api/player/3d0f648b-1cc2-45ab-a3b2-ea38f11f2675/embedded",
     "https://api.cappasity.com/api/player/b565d6d6-3153-40b7-9391-e334d7b71129/embedded",
-    "https://api.cappasity.com/api/player/b3cc1fd4-2e65-4650-ac1f-d4b1403ac276/embedded",
-    "https://api.cappasity.com/api/player/3d0f648b-1cc2-45ab-a3b2-ea38f11f2675/embedded"
+    "https://api.cappasity.com/api/player/5e09c628-8f54-4f02-bfd1-d2c6ab0baf12/embedded"
   ];
 
   const commonIframeParams = "?autorun=1&closebutton=0&logo=0&analytics=1&uipadx=0&uipady=0&enablestoreurl=0&hidehints=0&language=&autorotate=0&hidefullscreen=1&hideautorotateopt=1&hidesettingsbtn=1&enableimagezoom=1&zoomquality=1&hidezoomopt=1&arbutton=1";
